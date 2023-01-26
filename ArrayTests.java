c498c821d6d5dd17c6c5bb98b33097ddd4f1ec37
gavinzhang314 <122562285+gavinzhang314@users.noreply.github.com>
2023-01-26 10:51:36 -0800

    Implemented tests and fixed bugs for methods in ArrayExamples

--- ArrayTests.java
+++ ArrayTests.java
@@ -4,15 +4,24 @@ import org.junit.*;
 public class ArrayTests {
 	@Test 
 	public void testReverseInPlace() {
-    int[] input1 = { 3 };
+    int[] input1 = { 3, 4, 5, 6, 7, 8, 9 };
     ArrayExamples.reverseInPlace(input1);
-    assertArrayEquals(new int[]{ 3 }, input1);
+    assertArrayEquals(new int[]{ 9, 8, 7, 6, 5, 4, 3 }, input1);
 	}
 
 
   @Test
   public void testReversed() {
-    int[] input1 = { };
-    assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+    int[] input1 = { 10, 20, 30, 40, 50, 60, 70 };
+    assertArrayEquals(new int[]{ 70, 60, 50, 40, 30, 20, 10 }, ArrayExamples.reversed(input1));
+  }
+
+  @Test
+  public void testAverageWithoutLowest() {
+    double[] input1 = { 1, 2, 3, 4, 5 };
+    assertEquals(14 / 4.0, ArrayExamples.averageWithoutLowest(input1), 0.01);
+
+    double[] input2 = { 1, 1, 2, 3, 4, 5};
+    assertEquals(3.0, ArrayExamples.averageWithoutLowest(input2), 0.01);
   }
 }
